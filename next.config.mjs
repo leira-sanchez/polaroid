bb2a7f0251a8cac8a5ae2481f911c2903e17e007
Leira Sánchez <47611727+leira-sanchez@users.noreply.github.com>
2024-07-04 17:30:56 -0400

    add async rewrites to next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "/api/:path*", // Proxy to API
+      },
+    ];
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
