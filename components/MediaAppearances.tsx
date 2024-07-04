2f20825575a362ff4f910d8c41137caca1552940
Leira C Sánchez Quiñones <leira.sq@gmail.com>
2024-07-04 11:54:57 -0400

    --wip-- [skip ci]

---
+++ components/MediaAppearances.tsx
@@ -0,0 +1,13 @@
+import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+
+const MediaAppearances = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Media Appearances</CardTitle>
+        <CardDescription>Podcasts, Events, TV, Radio, etc.</CardDescription>
+      </CardHeader>
+      <ul></ul>
+    </Card>
+  );
+};
