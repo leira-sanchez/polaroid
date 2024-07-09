5ba808aef56118d6626849381237300a3cc3e03e
Leira Sánchez <47611727+leira-sanchez@users.noreply.github.com>
2024-07-09 12:04:57 -0400

    hotjar-2 (#10)

---
+++ components/HotJar.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Hotjar from "@hotjar/browser";
+import Script from "next/script";
+
+const siteId = 5053419;
+const hotjarVersion = 6;
+
+const HotJar = () => {
+  if (siteId) return null;
+  return (
+    <div>
+      <Script id="hotjar-snippet">
+        {`
+          (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:${siteId},hjsv:${hotjarVersion}};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+      </Script>
+    </div>
+  );
+};
+
+export default HotJar;
