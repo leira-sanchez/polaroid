98c1fb35e97abf77792f6be3ff45b58fef16134a
Leira C Sánchez Quiñones <leira.sq@gmail.com>
2026-03-20 21:49:16 -0700

    add ai

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -12,7 +12,8 @@ const Skills = () => {
         <div className="w-full">
           <div className="space-y-1">
             <p className="text-sm text-muted-foreground">
-              Software Engineering, Web Development and other technical skills
+              Software Engineering, Web Development, AI, and other technical
+              skills
             </p>
           </div>
 
